9e6003b98b7b5bfffae3f3ea99e18d1883ec9a33
David Reidsma <david.reidsma@gmail.com>
2021-11-11 23:55:15 -0500

    added data service

---
+++ helloCampusServer.js
@@ -0,0 +1,55 @@
+// Set up the database connection.
+const pgp = require('pg-promise')();
+const db = pgp({
+    host: process.env.DB_SERVER,
+    port: process.env.DB_PORT,
+    database: process.env.DB_USER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
+
+// Configure the server and its routes.
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const router = express.Router();
+router.use(express.json());
+
+router.get("/", readHelloMessage);
+router.get("/pointsofinterest", readPointsOfInterest);
+
+app.use(router);
+app.use(errorHandler);
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// Implement the CRUD operations.
+
+function errorHandler(err, req, res) {
+    if (app.get('env') === "development") {
+        console.log(err);
+    }
+    res.sendStatus(err.status || 500);
+}
+
+function returnDataOr404(res, data) {
+    if (data == null) {
+        res.sendStatus(404);
+    } else {
+        res.send(data);
+    }
+}
+
+function readHelloMessage(req, res) {
+    res.send('Hello, Hello Campus service!');
+}
+
+function readPointsOfInterest(req, res, next) {
+    db.many("SELECT * FROM pointofinterest")
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            next(err);
+        })
+}

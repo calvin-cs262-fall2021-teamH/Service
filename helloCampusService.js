21703367c2aba33f0f5ba1afe1f68070bfb96f0a
David Reidsma <david.reidsma@gmail.com>
2021-11-18 22:27:16 -0500

    swapped res.send with returnDataOr404 to fix a bug... probably

--- helloCampusService.js
+++ helloCampusService.js
@@ -88,7 +88,7 @@ function readQuestionsAtPoint(req, res, next) {
     db.many(
 "SELECT * FROM question WHERE question.pointid = ${pointid}", req.params)
         .then(data => {
-            res.send(data);
+            returnDataOr404(res, data);
         })
         .catch(err => {
             next(err);
